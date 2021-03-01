7719aec82dcc163c953ff2b7e837933854eccc91
Reginaldo Motta <reginaldomcv@gmail.com>
2021-03-01 13:45:26 -0400

    Parte 4

--- script.js
+++ script.js
@@ -7,21 +7,31 @@ let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
-}
+};
 
 let direction = "right";
 
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
-}
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+};
 
-function criarCobrinha() {
+function createBG() {
+    context.fillStyle = "green";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+};
+
+function createSnake() {
     for (i = 0; 1 < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "red";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
-}
+};
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+};
 
 document.addEventListener('keydown', update);
 
@@ -30,9 +40,9 @@ function update(event) {
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
     if (event.keyCode == 40 && direction != "up") direction = "down";
-}
+};
 
-function iniciarJogo() {
+function playGame() {
 
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
@@ -42,8 +52,9 @@ function iniciarJogo() {
 
 
 
-    criarBG();
-    criarCobrinha();
+    createBG();
+    createSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -58,9 +69,9 @@ function iniciarJogo() {
     let newHead = {
         x: snakeX,
         y: snakeY
-    }
+    };
 
     snake.unshift(newHead);
-}
+};
 
-let jogo = setInterval(iniciarJogo, 100);
+let game = setInterval(playGame, 100);
